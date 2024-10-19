4c7a554548f76c901f62c8cd584e40c5ff2144c4
Nikita <i_lossmydata@icloud.com>
2024-10-19 19:08:33 +1000

    Header and navbar

---
+++ frontend/affiche/src/constants/images.js
@@ -0,0 +1,13 @@
+export const logoPath = "/assets/logo.svg";
+export const likeInactive = "/assets/like-button-inactive.svg";
+
+export const categoryIcons = [
+    "/assets/icons/category-icons/icon-mic.svg",                        // "Концерты",
+    "/assets/icons/category-icons/icon-theater.svg",                    // "Театры",
+    "/assets/icons/category-icons/icon-kid.svg",                        // "Детям",
+    "/assets/icons/category-icons/icon-standup.svg",                    // "Стендап",
+    "/assets/icons/category-icons/icon-sport.svg",                      // "Спорт",
+    "/assets/icons/category-icons/icon-city.svg",                       // "В городе",
+    "/assets/icons/category-icons/icon-museum.svg",                     // "Музеи и галлереи",
+    "/assets/icons/category-icons/icon-arrowdown.svg",                  // "Ещё"
+]
