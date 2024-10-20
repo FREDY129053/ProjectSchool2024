d0b856f9ce91fa6b74626bf861598dfed4bcfbb3
Nikita <i_lossmydata@icloud.com>
2024-10-20 19:50:27 +1000

    Navbar carousel

--- frontend/affiche/src/layouts/navbar/navbar.js
+++ frontend/affiche/src/layouts/navbar/navbar.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import './navbar.css';
 import { NavbarItem } from './components/navbar-item/';
-
+import { ScrollingCarousel } from '@trendyol-js/react-carousel';
 
 function Navbar({ items }) {
-    const itemsToShow = items.slice(0, items.findIndex(item => item === 'Музеи и галлереи'));
+    const itemsToShow = items.slice(0, items.findIndex(item => item === 'Обучение'));
     
     return (
         <div className='Navbar'>
-            {itemsToShow.map((itemName, index) => (
+            <ScrollingCarousel className='NavbarCarousel'>
+                <div className='IndentLeft'></div>
+                {itemsToShow.map((itemName, index) => (
                 <NavbarItem
                     itemName={itemName}
                     key={index}
                 />
-            ))}
-            <NavbarItem
-                itemName={"еще"}
-                key={itemsToShow.length}
-            />
+                ))}
+                <NavbarItem
+                    itemName={"еще"}
+                    key={itemsToShow.length}
+                />
+                <div className='IndentRight'></div>
+            </ScrollingCarousel>
+            
         </div>
     );
 }
