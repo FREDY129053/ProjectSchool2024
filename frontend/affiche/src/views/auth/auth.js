e379ffaf28c8a62a06007c1e8ab65aebfa201ba9
saldaevvladimir <caldaevvlad3@gmail.com>
2024-10-23 02:03:55 +1000

    removed examples from auth view

--- frontend/affiche/src/views/auth/auth.js
+++ frontend/affiche/src/views/auth/auth.js
@@ -2,44 +2,10 @@ import React from 'react';
 import './auth.css';
 import { Header } from '../../layouts/header';
 import { AuthForm } from './components/auth-form';
-import { FilterMenu } from '../../components/common/filter-menu';
 
 
 
 function Auth() {
-
-    const filters = [
-        'балет',
-        'кино',
-        'опера',
-        'вечеринка',
-        'встреча',
-        'интенсив',
-        'спорт',
-        'концерт',
-        'балет',
-        'кино',
-        'опера',
-        'вечеринка',
-        'встреча',
-        'интенсив',
-        'спорт',
-        'концерт',
-    ]
-
-    const menuStyle = {
-        width: '200px',
-
-    }
-
-    const addFilter = (filter) => {
-        console.log('Add filter:', filter);
-    };
-
-    const removeFilter = (filter) => {
-        console.log('Remove filter:', filter);
-    };
-
 
     return (
         <div className='Auth'>
@@ -48,17 +14,7 @@ function Auth() {
                 userControls={false}
             />
 
-            {/* раскоментить когда закончится верстка менюшки фильтров */}
-            {/* <AuthForm /> */}
-
-            {/* это временно, чтобы видеть верстку */}
-            <FilterMenu 
-                style={menuStyle}
-                title={'Теги'}
-                filters={filters}
-                addFilter={addFilter}
-                removeFilter={removeFilter}
-            />
+            <AuthForm />
         </div>
     );
 }
